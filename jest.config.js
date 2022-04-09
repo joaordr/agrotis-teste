9ec9a8923f8e85f1d59bf527848aaf8601caee8b
João Rodrigo Prieto <jrp2033@gmail.com>
2022-04-09 14:20:12 -0300

    jest setup

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    testPathIgnorePatterns: ["/node_modules/"],
+    setupFilesAfterEnv: [
+        "<rootDir>/src/tests/setupTests.js"
+    ],
+    transform: {
+        "^.+\\.(js|jsx)$": "<rootDir>/node_modules/babel-jest"
+    },
+    testEnvironment: 'jsdom',
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "src/**/*.jsx",
+        "!src/**/*.spec.jsx",
+        "!src/**/_app.jsx",
+        "!src/**/_document.jsx",
+    ],
+    coverageReporters: ["lcov", "json"]
+};
